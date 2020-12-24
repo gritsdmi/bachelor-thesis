31ac5f2ea4cb13d437cca9842d9d5cb05adfae62
gritsdmi <dimagr13@ya.ru>
2020-12-24 02:27:30 +0100

    initial code commit

---
+++ bachelor_thesis_backend/src/main/java/cz/cvut/fel/bachelor_thesis/controllers/ErrorMappingController.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.bachelor_thesis.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(produces = {"application/json; charset=UTF-8"})
+public class ErrorMappingController {
+
+    @GetMapping
+    public String errorLabel() {
+        return "This is error page";
+    }
+}
