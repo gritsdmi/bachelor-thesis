e893d920e7eb7f1c32e6ede17170d82b78ae1efc
gritsdmi <dimagr13@ya.ru>
2021-03-02 13:30:04 +0100

    rename project dir bachelor_thesis_backend -> bachelor_thesis_sources

--- bachelor_thesis_sources/src/test/java/cz/cvut/fel/bachelor_thesis/BachelorThesisApplicationTests.java
+++ bachelor_thesis_sources/src/test/java/cz/cvut/fel/bachelor_thesis/BachelorThesisApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BachelorThesisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
