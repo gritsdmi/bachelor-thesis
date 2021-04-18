325fcac24903a81bb21344f48e056fe58001b764
gritsdmi <dimagr13@ya.ru>
2021-04-19 00:57:47 +0200

    first build deployed successfully

--- bachelor_thesis_sources/src/main/frontend/src/App.js
+++ bachelor_thesis_sources/src/main/frontend/src/App.js
@@ -1,37 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import CommissionsListPage from "./pages/manager/CommissionsList"
-import Header from "./components/Header";
-import {Redirect, Route, Switch} from 'react-router-dom';
-import Container from "@material-ui/core/Container";
-import AutoGeneratingPage from "./pages/manager/AutoGenerating";
-import ManualCreatingPage from "./pages/manager/ManualCreating";
-import ManageTeachersPage from "./pages/manager/ManageTeachers";
-import TeacherOverview from "./pages/teacher/TeacherOverview";
-import TeacherSettings from "./pages/teacher/TeacherSettings";
-import EmailTemplates from "./pages/manager/EmailTemplates";
-import Login from "./pages/login/Login";
-
 
 function App() {
-    return (
-        <>
-            <Container>
-                <Header/>
-                <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-                    <Route exact path="/" render={() => <Redirect to={"/commissions"}/>}/>
-                    <Route exact path="/login" component={Login}/>
-                    <Route exact path="/commissions" component={CommissionsListPage}/>
-                    <Route exact path="/auto" component={AutoGeneratingPage}/>
-                    <Route exact path="/manual" component={ManualCreatingPage}/>
-                    <Route exact path="/manage" component={ManageTeachersPage}/>
-                    <Route exact path="/teacher" component={TeacherOverview}/>
-                    <Route exact path="/teacher/settings" component={TeacherSettings}/>
-                    <Route exact path="/emails" component={EmailTemplates}/>
-                </Switch>
-            </Container>
-        </>
-
-    );
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+  );
 }
 
 export default App;
--- bachelor_thesis_sources/src/main/java/cz/cvut/fel/fem/controllers/Controller.java
+++ bachelor_thesis_sources/src/main/java/cz/cvut/fel/fem/controllers/Controller.java
@@ -1,7 +1,5 @@
 package cz.cvut.fel.fem.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public interface Controller {
 }
