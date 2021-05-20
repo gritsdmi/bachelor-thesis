91942c3bb28082f315eb4d2503a13bf9aaa1930c
gritsdmi <dimagr13@ya.ru>
2021-05-20 23:50:03 +0200

    upgrade xlsx download

--- bachelor_thesis_sources/src/main/frontend/src/utils/request.js
+++ bachelor_thesis_sources/src/main/frontend/src/utils/request.js
@@ -62,7 +62,7 @@ export function download(url, payload) {
                 const url = window.URL.createObjectURL(new Blob([response.data]));
                 const link = document.createElement('a');
                 link.href = url;
-                link.setAttribute('download', 'table.csv');
+                link.setAttribute('download', 'table.xlsx');
                 document.body.appendChild(link);
                 link.click();
             });
--- bachelor_thesis_sources/src/main/java/cz/cvut/fel/fem/utils/csv/CSVCreator.java
+++ bachelor_thesis_sources/src/main/java/cz/cvut/fel/fem/utils/csv/CSVCreator.java
@@ -4,13 +4,14 @@ import cz.cvut.fel.fem.model.Commission;
 import cz.cvut.fel.fem.services.CommissionService;
 import cz.cvut.fel.fem.to.GenerateCSVRequestTO;
 import lombok.extern.java.Log;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDate;
@@ -27,7 +28,7 @@ public class CSVCreator {
     @Autowired
     private CommissionService commissionService;
 
-    private final String fileNameLocal = "src/main/resources/new.csv";
+    private final String fileNameLocal = "src/main/resources/new.xlsx";
     private final String fileNameDeploy = "../webapps/fem/WEB-INF/classes/new.xlsx";
 
 //        private String fileName = fileNameLocal;
@@ -56,7 +57,7 @@ public class CSVCreator {
         }
 
         try {
-            var filename = createCSVFile(data);
+            var filename = createCSVFile(data, request);
             return new File(filename);
         } catch (IOException e) {
             e.printStackTrace();
@@ -65,25 +66,62 @@ public class CSVCreator {
     }
 
 
-    private String createCSVFile(List<Commission> data) throws IOException {
+    private String createCSVFile(List<Commission> data, GenerateCSVRequestTO request) throws IOException {
 
         final String[] HEADERS = {"id", "T1", "T2", "T3", "T4", "T5", "Semester", "Date", "Time", "Degree", "Study field"};
 
-        FileWriter out = new FileWriter(fileName);
-        try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS))) {
+        Workbook workbook = new XSSFWorkbook();
+        log.warning(request.toString());
+        Sheet sheet = workbook.createSheet(request.getSemester() != null ? request.getSemester() : "Sheet 1");
+        sheet.setColumnWidth(0, 6000);
+        sheet.setColumnWidth(1, 4000);
 
-            AtomicInteger id = new AtomicInteger();
-            data.forEach(comm -> {
-                id.getAndIncrement();
-                try {
-                    printer.printRecord(line(id, comm));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+        AtomicInteger rowCounter = new AtomicInteger();
+        Row header = sheet.createRow(rowCounter.getAndIncrement());
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        //filling header
+        for (int i = 0; i < HEADERS.length; i++) {
+            var headerCell = header.createCell(i);
+            headerCell.setCellValue(HEADERS[i]);
+            headerCell.setCellStyle(headerStyle);
         }
+///////
+
+        data.forEach(commission -> {
+            writeRow(commission, rowCounter.get(), sheet, workbook);
+            rowCounter.getAndIncrement();
+        });
+
+        FileOutputStream outputStream = new FileOutputStream(fileName);
+        workbook.write(outputStream);
+        workbook.close();
+
+
+//        FileWriter out = new FileWriter(fileName);
+//        try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS))) {
+//
+//            AtomicInteger id = new AtomicInteger();
+//            data.forEach(comm -> {
+//                id.getAndIncrement();
+//                try {
+//                    printer.printRecord(line(id, comm));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return fileName;
     }
 
@@ -105,5 +143,48 @@ public class CSVCreator {
         ret.add(commission.getExam().getDegree().toString());
         ret.add(commission.getExam().getFieldOfStudy());
         return ret;
+    }
+
+    private void writeRow(Commission commission, int rowCounter, Sheet sheet, Workbook workbook) {
+        var celCounter = 0;
+        var row = sheet.createRow(rowCounter);
+        CellStyle style = workbook.createCellStyle();
+        style.setWrapText(true);
+
+        Cell cell;
+
+        for (int i = 0; i < 5; i++) {
+            cell = row.createCell(celCounter++);
+            if (commission.getTeachers().size() > i) {
+                var t = commission.getTeachers().get(i);
+                if (t != null) {
+                    cell.setCellValue(t.getFullName());
+                }
+            } else {
+                cell.setCellValue("");
+            }
+            cell.setCellStyle(style);
+        }
+
+        cell = row.createCell(celCounter++);
+        cell.setCellValue(commission.getExam().getSemester());
+        cell.setCellStyle(style);
+
+        cell = row.createCell(celCounter++);
+        cell.setCellValue(commission.getExam().getDate());
+        cell.setCellStyle(style);
+
+        cell = row.createCell(celCounter++);
+        cell.setCellValue(commission.getExam().getTime());
+        cell.setCellStyle(style);
+
+        cell = row.createCell(celCounter++);
+        cell.setCellValue(commission.getExam().getDegree().toString());
+        cell.setCellStyle(style);
+
+        cell = row.createCell(celCounter++);
+        cell.setCellValue(commission.getExam().getFieldOfStudy());
+        cell.setCellStyle(style);
+
     }
 }
